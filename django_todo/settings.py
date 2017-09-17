430b3c0412f2db08984c0d62cbe3e6723e0d5c68
Diagnost1X <diagnostix@me.com>
2017-09-17 10:05:20 +0100

    Add database support

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import datetime
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -41,6 +43,10 @@ INSTALLED_APPS = [
     'rest_framework',
     'todo',
 ]
+
+DATABASES = {
+    'default': dj_database_url.config('CLEARDB_DATABASE_URL')
+}
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
